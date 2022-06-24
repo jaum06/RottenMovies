46797d6f1e99d8c008a52d0267f93fa54774e137
Andres Urrego <j.a.u.m.06@hotmail.com>
2022-06-24 15:57:30 -0500

    commit

--- app/src/main/java/com/zlasher/rottenmovies/MainActivity.kt
+++ app/src/main/java/com/zlasher/rottenmovies/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.zlasher.rottenmovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+//API KEY 8bde5af3f92f758581274c47ab874d13
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
